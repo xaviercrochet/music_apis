1cae713af2f7fdb934c8ff85dd9e3e47c55bdccc
Xavier Crochet <xacrochet@gmail.com>
2014-04-10 13:10:54 +0200

    generated itunes search url

--- music_apis/itunes/track.rb
+++ music_apis/itunes/track.rb
@@ -7,9 +7,9 @@ module MusicApis
     module Track
 
       def self.search(track_info)
-        request = ''
-        response = ''
-        #response = open(URI.escape(request)).read
+        request = 'https://itunes.apple.com/search?term='+track_info[:title].to_s+'&entity=musicTrack'
+        response = open(URI.escape(request)).read
+        p URI.escape(request).to_s
         return MusicApis::Itunes::Responses::TrackParser.new response
       end
     end
